32e0a348d661863e3cb77ab8dbec6a784ffcf8ef
ElTris <tristanromero026@gmail.com>
2017-11-19 18:48:44 -0600

    Archivos actualizados

---
+++ Calculadora_BHDO/src/calculadora_bhdo/Calculadora_BHDO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora_bhdo;
+
+/**
+ *
+ * @author PREDATOR
+ */
+public class Calculadora_BHDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface iinterface=new Interface();
+        iinterface.setVisible(true);
+        iinterface.setResizable(false);
+        
+        
+    }
+    
+}
